7817981b7093ae9bdb7cef061f736f0ff654ed52
Ana Alcantara <135041722+8098125043@users.noreply.github.com>
2024-01-08 13:23:09 -0400

    f

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hola , World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hola , MUNDO!");
+    }
+}
+
+
+
+
--- obj/Debug/net8.0/CSharpPractica.github.io.AssemblyInfo.cs
+++ obj/Debug/net8.0/CSharpPractica.github.io.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharpPractica.github.io")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94e4b972b97931a2d917cc99cfbca6f4c3a96115")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharpPractica.github.io")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharpPractica.github.io")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
